70078f9a69fe9da83cd0c22c08fba7dfa8072f88
Mikhail Kaplan <misha.kaplan1997@gmail.com>
2023-01-03 14:40:25 +0200

    Homework 3, есть ошибка вывода результата при введении неправильного оператора

---
+++ src/homework/Homework3.java
@@ -0,0 +1,46 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Homework3 {
+    public static void main(String[] args) {
+       getResult();
+    }
+    public static void getResult(){
+        Scanner scanner = new Scanner(System.in);
+
+        double result = 0;
+        System.out.println("Enter first number:");
+        double firstNum = scanner.nextDouble();
+        System.out.println("Enter one arithmetic operation (+, -, *, /, pow, %");
+        String operation = scanner.next();
+        System.out.println("Enter second number:");
+        double secondNum = scanner.nextDouble();
+
+        switch (operation) {
+            case "+":
+                result = firstNum + secondNum;
+                break;
+            case "-":
+                result = firstNum - secondNum;
+                break;
+            case "*":
+                result = firstNum * secondNum;
+                break;
+            case "/":
+                result = firstNum / secondNum;
+                break;
+            case "pow":
+                result = Math.pow(firstNum, secondNum);
+                break;
+            case "%":
+                result = firstNum % secondNum;
+                break;
+            default:
+                System.out.println("You enter wrong arithmetic operation");
+                getResult();
+                break;
+        }
+        System.out.println("Result: " + result);
+    }
+}
