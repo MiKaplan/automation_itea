d3d0e1a20ccd2a484e762bdf898ad9d960ae5be7
Mikhail Kaplan <misha.kaplan1997@gmail.com>
2023-01-18 10:44:20 +0200

    Homework 6

--- src/homework/Homework6.java
+++ src/homework/Homework6.java
@@ -2,12 +2,12 @@ package homework;
 
 public class Homework6 {
     public static void main(String[] args) {
-        int[][] array = MultipleArrays();
+        int[][] array = multipleArrays();
 
-        MaxNumInArray(array);
+        maxNumInArray(array);
     }
 
-    static int[][] MultipleArrays() {
+    static int[][] multipleArrays() {
         int[][] firstArray = {{1, 2}, {3, 4}};
         int[][] secondArray = {{5, 6}, {7, 8}};
         int [][] resultArray = new int[2][2];
@@ -27,7 +27,7 @@ public class Homework6 {
 
         return resultArray;
     }
-    static void MaxNumInArray(int[][] array) {
+    static void maxNumInArray(int[][] array) {
         int max = array[0][0];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
